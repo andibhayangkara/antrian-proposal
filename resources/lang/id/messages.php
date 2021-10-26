e4b926c8167a15a64db215885dcd4dacebea44dc
Your Name <you@example.com>
2021-10-26 19:14:52 +0800

    upload

---
+++ resources/lang/id/messages.php
@@ -0,0 +1,180 @@
+<?php
+
+return [
+    'save' => 'menyimpan',
+    'select' => 'Pilih',
+    'add' => 'menambahkan',
+    'name' => 'nama',
+    'actions' => 'tindakan',
+    'edit' => 'mengedit',
+    'delete' => 'menghapus',
+    'update' => 'Simpan',
+    'change' => 'perubahan',
+    'date' => 'tanggal',
+    'go' => 'pergi',
+    'range' => 'jarak',
+    'report' => 'melaporkan',
+    'month' => 'bulan',
+    'all' => 'semua',
+    'starting' => 'mulai',
+    'ending' => 'akhir',
+    'time' => 'waktu',
+    'issue' => 'Ambil',
+    'new' => 'baru',
+    'settings' => 'Pengaturan',
+    'default' => 'default',
+    'set' => 'set',
+    'language' => 'bahasa',
+    'next_only' => 'Berikutnya',
+    'cancel' => 'Membatalkan',
+    'type' => 'Mengetik',
+    'types' => 'jenis',
+    'serving' => 'Porsi',
+    'served' => 'Melayani',
+    'start' => 'Mulai',
+    'end' => 'Akhir',
+    'and' => 'Dan',
+    'in_seconds' => 'dalam Detik',
+    'avg_time' => 'Rata-rata waktu tunggu',
+    'min_time' => 'Minimum Menunggu Waktu',
+    'max_time' => 'Maksimum Menunggu Waktu',
+    'count_perday' => 'Jumlah total per hari',
+    'font_size' => 'Ukuran huruf',
+    'color' => 'Warna',
+    'today_queue' => 'Antrian Hari Ini',
+    'today_missed' => 'Antrian Yang Terlewat',
+    'today_served' => 'Antrian yang Dilayani Hari Ini',
+    'over_time' => 'Belum dilayani',
+    'today_yesterday' => 'Hari ini vs Kemarin',
+    'queue_details' => 'Detail antrian',
+    'more_info' => 'Info lebih lanjut',
+
+    'locales' => [
+        'id' => [
+            'gb' => 'English',
+            'tr' => 'Türk',
+            'sa' => 'العربية',
+            'fr' => 'français',
+            'de' => 'Deutsch',
+            'in' => 'हिन्दी',
+            'it' => 'italiano',
+            'pt' => 'português',
+            'ru' => 'русский',
+            'sk' => 'slovenský',
+            'es' => 'Español',
+            'th' => 'ไทย',
+            'id' => 'Indonesia',
+        ],
+    ],
+
+    'mainapp' => [
+        'select_language' => 'Pilih bahasa',
+        'display_url' => 'Tampilan Layar',
+        'issue_url' => 'Ambil Antrian',
+        'last_login' => 'login terakhir',
+        'profile' => 'Profil',
+        'logout' => 'keluar',
+        'menu' => [
+            'menu' => 'menu',
+            'dashboard' => 'Dasbor',
+            'call' => 'Panggilan',
+            'department' => 'Pelayanan',
+            'counter' => 'Loket',
+            'users' => 'Pengguna',
+            'reports' => [
+                'reports' => 'Laporan',
+                'user_report' => 'Laporan Pengguna',
+                'queue_list' => 'Daftar Antrian',
+                'monthly' => 'bulanan',
+                'statistical' => 'Statistik',
+                'missed' => 'terjawab',
+                'overtime' => 'Lembur',
+            ],
+            'account' => 'rekening',
+        ],
+
+        'role' => [
+            'Administrator' => 'Administrator',
+            'Staff' => 'Staf'
+        ],
+    ],
+
+    'dashboard' => [
+        'notification' => 'pemberitahuan',
+        'preview' => 'pratinjau',
+        'notification_text' => 'teks pemberitahuan',
+        'notification_placeholder' => 'pemberitahuan placeholder',
+    ],
+
+    'call' => [
+        'new_call' => 'Panggilan Baru',
+        'user' => 'Petugas',
+        'call_next' => 'Panggilan Berikutnya',
+        'click_department' => 'Pilih Jenis Pelayanan',
+        'todays_queue' => 'Antrian Hari Ini',
+        'number' => 'Nomor',
+        'called' => 'Panggil',
+        'recall' => 'Panggil Ulang',
+    ],
+
+    'department' => [
+        'letter' => 'surat',
+        'start' => 'mulai',
+    ],
+
+    'users' => [
+        'username' => 'nama pengguna',
+        'email' => 'e-mail',
+        'role' => 'peran',
+        'password' => 'kata sandi',
+        'confirm' => 'memastikan',
+    ],
+
+    'company' => [
+        'company' => 'perusahaan',
+        'address' => 'alamat',
+        'location' => 'tempat',
+        'phone' => 'telepon',
+    ],
+
+    'display' => [
+        'display' => 'Tampilan',
+        'token' => 'Tiket',
+        'please' => 'silahkan',
+        'proceed_to' => 'lanjutkan ke',
+    ],
+
+
+
+    // Dont want to change
+
+    'title' => 'Laravel Installer',
+    'next' => 'Next Step',
+    'finish' => 'Install',
+
+    'welcome' => [
+        'title'   => 'Welcome To The Installer',
+        'message' => 'Welcome to the setup wizard.',
+    ],
+
+    'requirements' => [
+        'title' => 'Requirements',
+    ],
+
+    'permissions' => [
+        'title' => 'Permissions',
+    ],
+
+    'environment' => [
+        'title' => 'Environment Settings',
+        'save' => 'Save .env',
+        'success' => 'Your .env file settings have been saved.',
+        'errors' => 'Unable to save the .env file, Please create it manually.',
+    ],
+
+    'final' => [
+        'title' => 'Finished',
+        'finished' => 'Application has been successfully installed.',
+        'exit' => 'Click here to exit',
+    ],
+];
